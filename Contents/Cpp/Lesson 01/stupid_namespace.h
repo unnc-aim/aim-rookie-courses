9bcf4ef44e198ff15232e9997a7a37e6df21ddd7
AnthonyBvvd <xlapostle@163.com>
2025-10-25 20:22:16 +0800

    courses code added

---
+++ Contents/Cpp/Lesson 01/stupid_namespace.h
@@ -0,0 +1,12 @@
+//
+// Created by Anon on 2025/10/25.
+//
+
+#pragma once // 防止头文件被重复包含
+
+#ifndef HELLOC_STUPID_NAMESPACE_H
+#define HELLOC_STUPID_NAMESPACE_H
+
+void namespace_test();
+
+#endif //HELLOC_STUPID_NAMESPACE_H
